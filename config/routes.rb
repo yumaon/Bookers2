d8fe9a369248530cd8ecf6fb184117e9848bca60
yumaon <110388364+yumaon@users.noreply.github.com>
2022-08-26 14:32:47 +0000

    [Add]viewファイルの作成まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
   root to: "homes#top"
   devise_for :users
+  get 'homes/about',as:'about'
+  
+  resources :books, only:[:index, :show, :edit]
+  
+  resources :users, only:[:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
