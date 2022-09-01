a67da86b216221fb6663c480a8826650597fb1c4
yumaon <110388364+yumaon@users.noreply.github.com>
2022-09-01 20:51:25 +0000

    [Add]新規投稿の失敗時のバリデージョンの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,9 +7,13 @@ class BooksController < ApplicationController
   def create
     @book=Book.new(book_params)
     @book.user_id=current_user.id
-    @book.save
-    flash[:notice] = "You have created book successfully."
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books=Book.all
+      render :index
+    end
   end
 
   def show
