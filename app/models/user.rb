57b905e47339041e2f34ea4724c844373d308293
yumaon <110388364+yumaon@users.noreply.github.com>
2022-09-01 22:03:41 +0000

    [Add]ユーザーのプロフィール更新が失敗したときのバリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
+  validates :name, presence: true
+
   def get_profile_image(width,height)
     unless profile_image.attached?
       file_path=Rails.root.join('app/assets/images/no_image.jpeg')
