d15afc23c0357cc14af7d43b5f35ea87bd3f5857
yumaon <110388364+yumaon@users.noreply.github.com>
2022-09-01 14:35:01 +0000

    [Add]userのeditページの作成、update機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
 
   resources :books, only:[:index, :create, :show, :edit, :update, :destroy]
 
-  resources :users, only:[:index, :show, :edit]
+  resources :users, only:[:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
