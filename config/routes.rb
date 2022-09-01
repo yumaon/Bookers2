ca176d70d965d4079a3c693adce5f8d93e97db5c
yumaon <110388364+yumaon@users.noreply.github.com>
2022-09-01 12:58:22 +0000

    [Add]bookのeditページの作成、update機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'homes/about',as:'about'
 
-  resources :books, only:[:index, :create, :show, :edit, :destroy]
+  resources :books, only:[:index, :create, :show, :edit, :update, :destroy]
 
   resources :users, only:[:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
