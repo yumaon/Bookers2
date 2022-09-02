f3304d07d882c26310d70bf453335ca25be6962d
yumaon <110388364+yumaon@users.noreply.github.com>
2022-09-02 00:15:26 +0000

    [Update]no imageのデフォルト画像の変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
 
   def get_profile_image(width,height)
     unless profile_image.attached?
-      file_path=Rails.root.join('app/assets/images/no_image.jpeg')
+      file_path=Rails.root.join('app/assets/images/m_e_others_470.png')
       profile_image.attach(io: File.open(file_path),filename:'default-image.jpg',content_type:'image/jpeg')
     end
     profile_image.variant(resize_to_limit:[width,height]).processed
